9fb232124dacd24f36d3cdf7b61917670a621f4f
alraro <alfonsoramosrojas@gmail.com>
2025-10-03 21:33:53 +0200

    Added basic user list query in user management page

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,7 +1,12 @@
 const db = require('../db');
 
-exports.getAllUsers = async () => {
-    const result = await db.query('SELECT * FROM users');
+exports.getAllUsers = async (name = "", page = 1, limit = 25) => {
+    let query = 'SELECT * FROM users WHERE name ILIKE $1';
+    let params = [`%${name}%`];
+
+    query += ` ORDER BY id LIMIT ${limit} OFFSET ${(page - 1) * limit}`;
+    
+    const result = await db.query(query, params);
     return result.rows;
 };
 
