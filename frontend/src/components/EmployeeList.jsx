44a32e28418ea2516ec9f88d08e9557fd62bd921
alraro <alfonsoramosrojas@gmail.com>
2025-10-07 13:36:27 +0200

    Tried making page cache but desisted

--- frontend/src/components/EmployeeList.jsx
+++ frontend/src/components/EmployeeList.jsx
@@ -2,51 +2,57 @@ import { useState, useEffect } from "preact/hooks";
 import UserBubble from "./EmployeeBubble";
 
 export default function EmployeeList() {
-    const [limit, setLimit] = useState(5);
-    const [page, setPage] = useState(1);
-    const [nameFilter, setNameFilter] = useState("");
-    const [employees, setEmployees] = useState([]);
+  const [limit, setLimit] = useState(5);
+  const [page, setPage] = useState(1);
+  const [nameFilter, setNameFilter] = useState("");
+  const [employees, setEmployees] = useState([]);
 
-    useEffect(() => {
-        async function fetchEmployees() {
-            let employees = await fetch(
-                `${import.meta.env.PUBLIC_BACKEND_URL}/employees?name=${nameFilter}&page=${page}&limit=${limit}`
-            )
-                .then((res) => res.json())
-                .catch(() => []);
-            if (employees.error) {
-                employees = [];
-            }
-            if (employees.length === 0) {
-                setPage(Math.max(page - 1, 1));
-            } else {
-                setEmployees(employees);
-            }
-        }
-        fetchEmployees();
-    }, [nameFilter, page, limit]);
+  async function fetchEmployees() {
+    let employees = await fetch(
+      `${import.meta.env.PUBLIC_BACKEND_URL}/employees?name=${nameFilter}&page=${page}&limit=${limit}`
+    )
+      .then((res) => res.json())
+      .catch(() => []);
+    if (employees.error) {
+      employees = [];
+    }
+    setEmployees(employees);
+  }
 
-    return (
-        <div id="user-list">
-            <input
-                type="text"
-                id="user-search"
-                placeholder="Search employees..."
-                value={nameFilter}
-                onInput={(e) => setNameFilter(e.target.value)}
-            />
-            <ul>
-                {employees.map((employee) => (
-                    <UserBubble
-                        name={employee.name}
-                        email={employee.email}
-                    />
-                ))}
-                <div id="pagination-controls">
-                <button class="pagination-button" id="previous-page-button" onClick={() => setPage(Math.max(page - 1, 1))}>Previous page</button>
-                <button class="pagination-button" id="next-page-button" onClick={() => setPage(page + 1)}>Next page</button>
-                </div>
-            </ul>
+  useEffect(() => {
+    fetchEmployees();
+  }, [nameFilter, page, limit]);
+
+  return (
+    <div id="user-list">
+      <input
+        type="text"
+        id="user-search"
+        placeholder="Search employees..."
+        value={nameFilter}
+        onInput={(e) => setNameFilter(e.target.value)}
+      />
+      <ul>
+        {employees.map((employee) => (
+          <UserBubble name={employee.name} email={employee.email} />
+        ))}
+        <div id="pagination-controls">
+          <button
+            class="pagination-button"
+            id="previous-page-button"
+            onClick={() => setPage(Math.max(page - 1, 1))}
+          >
+            Previous page
+          </button>
+          <button
+            class="pagination-button"
+            id="next-page-button"
+            onClick={() => setPage(page + 1)}
+          >
+            Next page
+          </button>
         </div>
-    );
+      </ul>
+    </div>
+  );
 }
